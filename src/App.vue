a49cd8173b3ec00cc8cf4d4861b10a12db1494ae
cag185 <cag185@pitt.edu>
2025-01-09 18:28:26 -0500

    removed about

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav class="flex justify-self-end text-xl py-4 px-8 lg:px-24">
         <RouterLink class="custom-router-link" to="/">Home</RouterLink>
-        <RouterLink class="custom-router-link" to="/about">About</RouterLink>
         <RouterLink class="custom-router-link" to="/projects">Projects</RouterLink>
         <RouterLink class="custom-router-link" to="/work-experience">Work Experience</RouterLink>
         <RouterLink class="custom-router-link" to="/resume">Resume</RouterLink>
--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="custom-container">
-    <div class="custom-title">About</div>
-  </div>
-</template>
