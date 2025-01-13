fa98b3cc5c8e1c0f06326d45274573eee1a2d390
cag185 <cag185@pitt.edu>
2025-01-12 19:51:52 -0500

    works

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -5,14 +5,37 @@
       <hr class="w-72 h-1 mx-auto my-2 bg-gray-300 border-0 rounded" />
     </div>
 
-    <div class="pl-5 pt-10 flex-col justify-left space-y-10">
-      <div class="">
-        <div class="custom-subtitle font-bold">SMS Group</div>
-        <div class="custom-information-text">Some info about SMS Group</div>
-      </div>
+    <div class="pl-5 pt-10 flex-col justify-left space-y-20">
       <div>
         <div class="custom-subtitle font-bold">The Restaurant Store</div>
-        <div class="custom-information-text">Some info about TRS</div>
+        <div class="custom-dates">May-2023 | Current</div>
+        <div class="custom-information-text">
+          I started off at the Restaurant store as an intern in software engineering. Over the
+          summer I was exposed to a multitude of projects, including our E-commerce website, the
+          Point Of Sales Machine, our API, and the administration management site. I was hired on as
+          a Full Stack software engineer before I gradutated college, and did part time work through
+          my final semeseter of college. Since graduation, I have helped with a complete overhaul of
+          the admin management site, with an updated tech stack to help produce cleaner and more
+          effecient code. I have had a hand in getting the project off the ground and the majority
+          of my work entails producing features for and providing support for this app. I also work
+          on the API side, using EF core and Dotnet to perform CRUD operations on the entities we
+          use to manage orders, accounts, users, and items. The majority of the tech stack I use on
+          a daily basis is Vue.js, Tailwind CSS, Docker, SQL database, C#/Dotnet, and Azure Dev ops
+          for Git control and task management.
+        </div>
+      </div>
+      <div>
+        <div class="custom-subtitle font-bold">SMS Group</div>
+        <div class="custom-dates">July-2021 | March-2022</div>
+        <div class="custom-information-text">
+          My time at SMS group as a computer engineering co-op taught me some basic front end
+          development skills. We also used C# and Dotnet for the backend, where I learned about
+          Model-View-viewModel patterns. I mostly helped work on the Human Machine interface, which
+          was a pretty large product that helped Steel Manufacturing plants control equipment that
+          our company created software to automate. I also worked on a few smaller excel VBA and
+          python scripts to help with our Task management, as well as migrate our workflow from
+          Gitea to Gitlabs.
+        </div>
       </div>
     </div>
   </div>
