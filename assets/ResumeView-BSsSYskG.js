79d957150c5c2c9a2befe3dacc5004ce61d4f71f
cag185 <cag185@pitt.edu>
2025-01-12 19:28:01 -0500

    Updates

---
+++ assets/ResumeView-BSsSYskG.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,b as s,o as c}from"./index-CrWeax6N.js";const a={},l={class:"custom-container space-y-4"};function n(r,e){return c(),t("div",l,e[0]||(e[0]=[s("div",{class:"custom-title"},"Resume",-1),s("embed",{src:"/Portfolio-v2/Caleb_Gibson_Resume_Fall_2024.pdf",type:"application/pdf",class:"w-full h-screen"},null,-1)]))}const _=o(a,[["render",n]]);export{_ as default};
