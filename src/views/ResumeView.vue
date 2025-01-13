9aa4a1989ffa66653e1500aa238dd7f508858f48
cag185 <cag185@pitt.edu>
2025-01-12 19:35:21 -0500

    resume

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="custom-container space-y-4">
+  <div class="custom-resume-container space-y-4">
     <div class="custom-title">Resume</div>
     <embed
-      :src="'/Portfolio-v2/public/Caleb_Gibson_Resume_Fall_2024.pdf'"
+      :src="'Caleb_Gibson_Resume_Fall_2024.pdf'"
       type="application/pdf"
       class="w-full h-screen"
     />
